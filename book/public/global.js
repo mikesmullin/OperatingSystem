5bacddadcf98fb010365f6edea1e8d82a9e84542
Mike Smullin <mike@smullindesign.com>
2018-03-10 20:38:17 -0700

    minor ui improvement

--- book/public/global.js
+++ book/public/global.js
@@ -15,17 +15,30 @@ var Hello = {
                         break;
                 }
             });
+        $('.description .tooltip', v.dom).hide().map((i,e) =>
+            $(e).prev('label').on('click', ()=>$(e).toggle()));
+        $('.group a.remove', v.dom).on('click', function() { $(this).closest('.b-g').slideUp({ duration: 250, easing: 'linear', complete: function(){ $(this).remove(); }}); });
     },
     view: function() {
-        const input = () => m('input', { maxlength: 1, value: 0 });
-        const group = ({size, color, label, description, children}) => m(`.group.bg-${color}`, [
-            m('.description', [
-                m('label', `${label}:`),
-                ` ${description}`
-            ]),
-        ].concat(children ? children : new Array(size).fill(0).map(v=>
-            input()
-        )));
+        const repeat = (count, e) => new Array(count).fill(0).map(e);
+        const mIf = (t, r) => t ? [r] : [];
+        const inputs = (id, count, remove) => {
+            return m(`.b-g.b-g-${id}`, [
+                repeat(count, (nil, i) =>
+                    m(`input.b.b-${i}`, { maxlength: 1, value: 0 })),                
+            ].concat(mIf(remove,
+                m('a.remove', { href: '#' }, m('i.fas.fa-fw.fa-minus-square.red.ml-1'))
+            )));
+        }
+        const group = ({label, description, children}) => m(`.group`, [
+                m('.description', [
+                    m('label', [
+                        `${label}: `,
+                        m('i.blue.fas.fa-info-circle.fa-sm')
+                    ]),
+                    m('.tooltip', `${description}`),
+                ]),
+            ].concat(children));
 
         return m('.container-fluid', [
             m('.row.mt-2', [
@@ -36,36 +49,77 @@ var Hello = {
             m('.row', [
                 m('.col-12',
                     m('.instruction',
-                        group({ size: 4*8, label: 'Prefix',
-                            description: '0, 1, 2, or 4 bytes of performance modifiers. (ie., repeat until, lock memory, bind cpu, branch hint, size overrides)' }),
-                        group({ size: 3*8, label: 'Opcode',
-                            description: '1, 2, or 3 byte instruction identifier.' }),
+                        group({ label: 'Prefix',
+                            description: '0, 1, 2, or 4 bytes of performance modifiers. (ie., repeat until, lock memory, bind cpu, branch hint, size overrides)',
+                            children: [
+                                inputs(1, 8, true),
+                                inputs(2, 8, true),
+                                inputs(3, 8, true),
+                                inputs(4, 8, true),
+                            ]}),
+                        group({ label: 'Opcode',
+                            description: '1, 2, or 3 byte instruction identifier.',
+                            children: [
+                                inputs(1, 8, false),
+                                inputs(2, 8, true),
+                                inputs(3, 8, true),
+                            ]}),
                         group({ label: 'Mod-R/M',
                             description: 'One byte addressing mode (register or memory) and operand size.',
                             children: [
-                                group({ size: 2, label: 'Mod',
-                                  description: 'Specifies x86 addressing mode.' }),
-                                group({ size: 3, label: 'Reg',
-                                    description: 'Specifies source or destination register.' }),
-                                group({ size: 3, label: 'R/M',
+                                group({ label: 'Mod',
+                                  description: 'Specifies x86 addressing mode.',
+                                  children: [
+                                      inputs(1, 2, false),
+                                  ]}),
+                                group({ label: 'Reg',
+                                    description: 'Specifies source or destination register.',
+                                    children: [
+                                        inputs(1, 3, false),
+                                    ]}),
+                                group({ label: 'R/M',
                                  description: `Combined with Mod, specifies either
                                     the second operand in a two-operand instruction, or
-                                    the only operand in a single-operand instruction.` }),
+                                    the only operand in a single-operand instruction.`,
+                                    children: [
+                                        inputs(1, 3, false),
+                                    ]}),
                             ]}),
                         group({ label: 'SIB (Scaled Index Byte)',
                             description: 'One byte scaled index memory address.',
                             children: [
-                                group({ size: 2, label: 'Scale',
-                                  description: '1, 2, 4, or 8 factor.' }),
-                                group({ size: 3, label: 'Index',
-                                    description: 'Index register.' }),
-                                group({ size: 3, label: 'Base',
-                                 description: `Base register.` }),
+                                group({ label: 'Scale',
+                                  description: '1, 2, 4, or 8 factor.',
+                                  children: [
+                                      inputs(1, 2, false),
+                                  ]}),
+                                group({ label: 'Index',
+                                    description: 'Index register.',
+                                    children: [
+                                        inputs(1, 3, false),
+                                    ]}),
+                                group({ label: 'Base',
+                                    description: `Base register.`,
+                                    children: [
+                                        inputs(1, 3, false),
+                                    ]}),
                             ]}),
-                        group({ size: 4*8, label: 'Displacement',
-                            description: '0 - 32 bit address offset.' }),
-                        group({ size: 4*8, label: 'Immediate',
-                            description: '0 - 32 bit constant.' }),
+                        group({ label: 'Displacement',
+                            description: '0 - 32 bit address offset.',
+                            children: [
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                            ]}),
+                        group({ label: 'Immediate',
+                            description: '0 - 32 bit constant.',
+                            children: [
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                                inputs(1, 8, false),
+                            ]}),
                     ),
                 ),
             ]),
